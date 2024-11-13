94748f97e586665adf62de5cf13bfb92eeea78fc
Mikk Martin <mikk.martin@gmail.com>
2024-11-13 15:32:18 +0200

    bg

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import SliderDemo from "./slider";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+
+        <SliderDemo />
 
         <div className={styles.ctas}>
           <a
---
+++ app/slider.tsx
@@ -0,0 +1,15 @@
+import * as Slider from "@radix-ui/react-slider";
+import styles from './slider.module.scss'
+
+const SliderDemo = () => (
+  <form>
+    <Slider.Root className={styles.Root} defaultValue={[50]} max={100} step={1}>
+      <Slider.Track className={styles.Track}>
+        <Slider.Range className={styles.Range} />
+      </Slider.Track>
+      <Slider.Thumb className={styles.Thumb} aria-label="Volume" />
+    </Slider.Root>
+  </form>
+);
+
+export default SliderDemo;
