378921d1a4c1cb98eba12f5e7d9807c284974d6f
Mikk Martin <mikk.martin@gmail.com>
2024-11-19 11:35:39 +0200

    Update page.tsx

--- app/slider/page.tsx
+++ app/slider/page.tsx
@@ -2,6 +2,9 @@ import { SliderV2 } from "./SliderV2";
 
 export default function Home() {
   return (
-    <SliderV2 labelSuffix="%" />
+    <div style={{ maxWidth: '20rem', display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+      <SliderV2 />
+      <SliderV2 labelSuffix="%" />
+    </div>
   );
 }
