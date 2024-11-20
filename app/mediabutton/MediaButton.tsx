7f4759a380912ded6fba9dd7ec7f509187eb0704
Mikk Martin <mikk.martin@gmail.com>
2024-11-20 09:56:40 +0200

    some slider updates etc

--- app/mediabutton/MediaButton.tsx
+++ app/mediabutton/MediaButton.tsx
@@ -1,5 +1,30 @@
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import styles from './MediaButton.module.scss';
+import { Mic, ChevronDown } from 'lucide-react'
 
 export function MediaButton() {
-  return <div className={styles.root}>Media Button</div>
+  return (
+    <SegmentedButtonDropdown>
+      <ButtonWithToolTip />
+    </SegmentedButtonDropdown>
+  )
+}
+
+function SegmentedButtonDropdown({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={styles.root}>
+      {children}
+      <button className={styles.button}><ChevronDown /></button>
+    </div>
+  )
+}
+
+function ButtonWithToolTip() {
+  return (
+    <div>
+      <div className={styles.testButton}>
+        <Mic />
+      </div>
+    </div>
+  )
 }
--- app/slider/page.tsx
+++ app/slider/page.tsx
@@ -2,7 +2,7 @@ import { SliderV2 } from "./SliderV2";
 
 export default function Home() {
   return (
-    <div style={{ maxWidth: '20rem', display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+    <div style={{ width: '100%', maxWidth: '20rem', display: 'flex', flexDirection: 'column', gap: '1rem' }}>
       <SliderV2 />
       <SliderV2 labelSuffix="%" />
     </div>
