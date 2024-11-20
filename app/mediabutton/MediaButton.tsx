808912994ab31b0bff04de4dcc3c48c892739ffb
Mikk Martin <mikk.martin@gmail.com>
2024-11-20 10:30:45 +0200

    motion

--- app/mediabutton/MediaButton.tsx
+++ app/mediabutton/MediaButton.tsx
@@ -1,30 +1,66 @@
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import styles from './MediaButton.module.scss';
 import { Mic, ChevronDown } from 'lucide-react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useState } from "react";
 
 export function MediaButton() {
   return (
-    <SegmentedButtonDropdown>
+    <div className={styles.testContainer}>
+      <SegmentedButtonDropdown>
+        <ButtonWithToolTip />
+      </SegmentedButtonDropdown>
+      <SegmentedButtonDropdown>
+        <ButtonWithToolTip />
+      </SegmentedButtonDropdown>
       <ButtonWithToolTip />
-    </SegmentedButtonDropdown>
+      <ButtonWithToolTip />
+    </div>
   )
 }
 
 function SegmentedButtonDropdown({ children }: { children: React.ReactNode }) {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <div className={styles.root}>
-      {children}
-      <button className={styles.button}><ChevronDown /></button>
+      <DropdownMenu.Root open={isOpen} onOpenChange={setIsOpen}>
+        {children}
+        <DropdownMenu.Trigger className={styles.button}>
+          <ChevronDown />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Portal forceMount>
+          <AnimatePresence>
+            {isOpen &&
+              <DropdownMenu.Content key="content" forceMount asChild>
+                <motion.div
+                  transition={{ duration: 0.2 }}
+                  initial={{ opacity: 0, scale: 0.9 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0, scale: 0.9 }}
+                >
+                  <DropdownMenu.RadioGroup>
+                    <DropdownMenu.RadioItem value="asdasd2">
+                      asdasd
+                    </DropdownMenu.RadioItem>
+                    <DropdownMenu.RadioItem value="asdasd2">
+                      asdasd
+                    </DropdownMenu.RadioItem>
+                  </DropdownMenu.RadioGroup>
+                </motion.div>
+              </DropdownMenu.Content>
+            }
+          </AnimatePresence>
+        </DropdownMenu.Portal>
+      </DropdownMenu.Root>
     </div>
   )
 }
 
 function ButtonWithToolTip() {
   return (
-    <div>
-      <div className={styles.testButton}>
-        <Mic />
-      </div>
-    </div>
+    <button className={styles.testButton}>
+      <Mic />
+    </button>
   )
 }
