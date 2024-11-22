1f270d24d3cc057bc57952aa5c3be7ad54e73c63
Mikk Martin <mikk.martin@gmail.com>
2024-11-22 16:55:18 +0200

    updates from prod

--- app/slider/SliderV2.tsx
+++ app/slider/SliderV2.tsx
@@ -2,7 +2,7 @@
 
 import { Root, Thumb, Track } from "@radix-ui/react-slider";
 import { motion, transform, useMotionValue, useSpring, useTransform } from 'framer-motion';
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import useMeasure from 'react-use-measure';
 import styles from './SliderV2.module.scss';
 import { clampNumber } from "../utils/utils";
@@ -30,68 +30,40 @@ export function SliderV2({ defaultValue = 50, min = 0, max = 100, step = 1, disa
   const initialValue = _value ?? defaultValue
   const [value, setValue] = useState(initialValue);
 
-  const initialXNormalizedProgress = valueToNormalized(initialValue)
+  const initialXNormalizedProgress = valueToNormalized(initialValue, min, max)
   const xNormalizedProgress = useMotionValue(initialXNormalizedProgress)
   const steppedProgress = useMotionValue(initialXNormalizedProgress)
 
   const layeredProgress = useTransform(xNormalizedProgress, (val: number) => (val + steppedProgress.get()) / 2)
   const spring = useSpring(layeredProgress, snappy)
 
-  function updateProgress(ev: React.PointerEvent<HTMLDivElement>) {
-    if (ev.buttons <= 0 || ev.target === ev.currentTarget) return
+  const [barRef, barSize] = useMeasure();
+  const [iconRef, iconSize] = useMeasure();
+  const [labelRef, labelSize] = useMeasure();
+
+  const isDragging = useRef(false);
+
+  const handleUpdateProgress = useCallback((ev: React.PointerEvent<HTMLDivElement>) => {
+    if (ev.buttons <= 0 || ev.target === ev.currentTarget || !isDragging.current) return
     const { left, width } = ev.currentTarget.getBoundingClientRect()
     const overflow = ev.clientX - left
     xNormalizedProgress.set(overflow / width);
-  }
+  }, [xNormalizedProgress]);
 
-  function valueToNormalized(value: number) {
-    return (value - min) / (max - min)
-  }
-
-  function fallOffX(val: number) {
-    const eased = transform(
-      val,
-      [1, MAX_FALLOFF_DISTANCE_NORMALIZED],
-      [1, 1.05],
-      {
-        ease: easeOutSine,
-        clamp: true
-      }
-    )
-    if (val > 1) return eased
-    return eased * -1
-  }
-
-  function fallOffY(val: number) {
-    return transform(val,
-      [1, 0],
-      [1, 0.9],
-      {
-        ease: (v: number) => Math.pow(v, 1.25),
-        clamp: true
-      })
-  }
-
-  function handleStopDragging() {
+  const handleStopDragging = useCallback(() => {
     const clamped = clampNumber(xNormalizedProgress.get(), 0, 1)
     const normalizedStep = step / max
     const snapped = Math.round(clamped / normalizedStep) * normalizedStep
     xNormalizedProgress.set(snapped)
     isDragging.current = false
     rest.onDragEnd?.()
-  }
-
-  const isDragging = useRef(false);
+  }, [xNormalizedProgress, step, max, rest.onDragEnd]);
 
   useEffect(() => {
     setValue(_value ?? defaultValue)
-    steppedProgress.set(valueToNormalized(_value ?? defaultValue))
-    xNormalizedProgress.set(valueToNormalized(_value ?? defaultValue))
+    steppedProgress.set(valueToNormalized(_value ?? defaultValue, min, max))
+    xNormalizedProgress.set(valueToNormalized(_value ?? defaultValue, min, max))
   }, [_value])
-
-  const [barRef, barSize] = useMeasure();
-  const [iconRef, iconSize] = useMeasure();
-  const [labelRef, labelSize] = useMeasure();
 
   const obstuctedPixels = useMemo(() => {
     return [iconSize, labelSize].reduce<[number, number][]>((acc, el) => {
@@ -103,9 +75,67 @@ export function SliderV2({ defaultValue = 50, min = 0, max = 100, step = 1, disa
     }, [])
   }, [barSize, iconSize, labelSize])
 
+  const transformFalloff = useCallback((val: number) => {
+    if (val > 1) return fallOffX(val)
+    if (val < 0) return fallOffX(1 + -val)
+    return 1
+  }, [])
+
+  const transformScaleY = useCallback((val: number) => {
+    if (val > 1) return fallOffY(2 - val)
+    if (val > 0) return 1
+    return fallOffY(1 + val)
+  }, [])
+
+  const handleValueChange = useCallback((_value: number[]) => {
+    const value = _value[0] ?? defaultValue
+    setValue(value)
+    steppedProgress.set(valueToNormalized(value, min, max))
+    xNormalizedProgress.set(valueToNormalized(value, min, max))
+    onValueChange?.(value)
+  }, [defaultValue, steppedProgress, xNormalizedProgress, onValueChange])
+
+  const handlePointerDown = useCallback((ev: React.PointerEvent<HTMLDivElement>) => {
+    if (disabled) return
+    isDragging.current = true
+    handleUpdateProgress(ev)
+    rest.onDragStart?.()
+  }, [disabled, handleUpdateProgress, rest.onDragStart]);
+
+  const transformOriginHandler = useCallback((val: number) =>
+    val > 0.5 ? 'left' : 'right'
+    , []);
+
+  const handleBarWidthTransform = useCallback((v: number) =>
+    `calc(${clampNumber(v, 0, 1) * 100}%`
+    , []);
+
+  const handleBorderRadiusTransform = useCallback((v: number) => {
+    const vPixels = v * (barSize.width ?? 0)
+    return vPixels <= 16 ? transform(vPixels, [8, 16], [0, 6], { clamp: true }) : 6
+  }, [barSize.width]);
+
+  const handleKnobPositionTransform = useCallback((normalizedValue: number) => {
+    const v = normalizedValue * (barSize.width ?? 0)
+    if (v <= 14) return 14 + KNOB_OFFSET
+    if (v >= barSize.width - 12) return barSize.width - 8
+    return v <= KNOB_OFFSET * 2 ? KNOB_OFFSET * -1 : v >= barSize.width ? barSize.width + KNOB_OFFSET : v + KNOB_OFFSET
+  }, [barSize.width]);
+
+  const handleKnobOpacityTransform = useCallback((normalizedValue: number) => {
+    if (!barSize.width) return 0
+    const val = normalizedValue * (barSize.width ?? 0)
+    return obstuctedPixels.some(([start, end]) => val > start && val < end) ? 0 : 1
+  }, [barSize.width, obstuctedPixels]);
+
+  const handleHitBoxTransform = useCallback((normalizedValue: number) => {
+    const valPx = normalizedValue * (barSize.width ?? 0)
+    return valPx < 20 ? Math.min(14 - valPx, 14) : 0
+  }, [barSize.width]);
+
   return (
     <SliderRoot
-      className={styles.Root}
+      className={styles.root}
       defaultValue={[defaultValue]}
       min={min}
       max={max}
@@ -113,69 +143,47 @@ export function SliderV2({ defaultValue = 50, min = 0, max = 100, step = 1, disa
       disabled={disabled}
       ref={barRef}
       style={{
-        x: useTransform(spring, (val: number) => val > 1 ? fallOffX(val) : val < 0 ? fallOffX(1 + -val) : 1),
-        scaleX: useTransform(spring, (val: number) => val > 1 ? fallOffX(val) : val < 0 ? fallOffX(1 + -val) : 1),
-        scaleY: useTransform(spring, (val: number) => val > 1 ? fallOffY(2 - val) : val > 0 ? 1 : fallOffY(1 + val)),
+        x: useTransform(spring, transformFalloff),
+        scaleX: useTransform(spring, transformFalloff),
+        scaleY: useTransform(spring, transformScaleY),
         //@ts-ignore
-        transformOrigin: useTransform(spring, (val: number) => val > 0.5 ? 'left' : 'right'),
+        transformOrigin: useTransform(spring, transformOriginHandler),
       }}
       value={[value]}
-      onValueChange={(_value: number[]) => {
-        const value = _value[0] ?? defaultValue
-        setValue(value)
-        steppedProgress.set(valueToNormalized(value))
-        xNormalizedProgress.set(valueToNormalized(value))
-        onValueChange?.(value)
-      }}
-      onPointerDown={(ev: React.PointerEvent<HTMLDivElement>) => {
-        if (disabled) return
-        isDragging.current = true
-        updateProgress(ev)
-        rest.onDragStart?.()
-      }}
-      onPointerMove={(ev: React.PointerEvent<HTMLDivElement>) => isDragging.current && updateProgress(ev)}
+      onValueChange={handleValueChange}
+      onPointerDown={handlePointerDown}
+      onPointerMove={handleUpdateProgress}
       onPointerUp={handleStopDragging}
       onPointerCancel={handleStopDragging}
     >
-      <Track className={styles.Track}>
+      <Track className={styles.track}>
         <motion.div
-          className={styles.Bar}
+          className={styles.bar}
           style={{
-            width: useTransform(spring, (v: number) => `calc(${clampNumber(v, 0, 1) * 100}%`),
-            borderRadius: useTransform(spring, (v: number) => {
-              const vPixels = v * (barSize.width ?? 0)
-              return vPixels <= 16 ? transform(vPixels, [8, 16], [0, 6], { clamp: true }) : 6
-            }),
+            width: useTransform(spring, handleBarWidthTransform),
+            borderRadius: useTransform(spring, handleBorderRadiusTransform),
           }}
         />
         <motion.div
-          className={styles.Knob}
+          className={styles.knob}
           style={{
-            x: useTransform(spring, (normalizedValue: number) => {
-              const v = normalizedValue * (barSize.width ?? 0)
-              if (v <= 14) return 14 + KNOB_OFFSET
-              if (v >= barSize.width - 12) return barSize.width - 8
-              return v <= KNOB_OFFSET * 2 ? KNOB_OFFSET * -1 : v >= barSize.width ? barSize.width + KNOB_OFFSET : v + KNOB_OFFSET
-            }),
-            opacity: useTransform(spring, (normalizedValue: number) => {
-              if (!barSize.width) return 0
-              const val = normalizedValue * (barSize.width ?? 0)
-              return obstuctedPixels.some(([start, end]) => val > start && val < end) ? 0 : 1
-            }),
+            x: useTransform(spring, handleKnobPositionTransform),
+            opacity: useTransform(spring, handleKnobOpacityTransform),
           }}
         />
-        {icon && <div ref={iconRef} className={styles.Icon}>{icon}</div>}
-        <motion.div ref={labelRef} className={styles.Label}>
+        {icon && <div ref={iconRef} className={styles.icon}>{icon}</div>}
+        <motion.div ref={labelRef} className={styles.label}>
           {value}
           {labelSuffix}
         </motion.div>
       </Track>
-      <HitBox style={{
-        x: useTransform(xNormalizedProgress, (normalizedValue: number) => {
-          const valPx = normalizedValue * (barSize.width ?? 0)
-          return valPx < 20 ? Math.min(14 - valPx, 14) : 0
-        }),
-      }} className={styles.HitBox} aria-label={rest['aria-label']} />
+      <HitBox
+        style={{
+          x: useTransform(xNormalizedProgress, handleHitBoxTransform),
+        }}
+        className={styles.hitBox}
+        aria-label={rest['aria-label']}
+      />
     </SliderRoot>
   )
 }
@@ -184,6 +192,34 @@ const HitBox = motion(Thumb)
 
 function easeOutSine(x: number): number {
   return Math.sqrt(1 - Math.pow(x - 1, 2))
+}
+
+function valueToNormalized(value: number, min: number, max: number) {
+  return (value - min) / (max - min)
+}
+
+function fallOffX(val: number) {
+  const eased = transform(
+    val,
+    [1, MAX_FALLOFF_DISTANCE_NORMALIZED],
+    [1, 1.05],
+    {
+      ease: easeOutSine,
+      clamp: true
+    }
+  )
+  if (val > 1) return eased
+  return eased * -1
+}
+
+function fallOffY(val: number) {
+  return transform(val,
+    [1, 0],
+    [1, 0.9],
+    {
+      ease: (v: number) => Math.pow(v, 1.25),
+      clamp: true
+    })
 }
 
 const SliderRoot = motion(Root)
