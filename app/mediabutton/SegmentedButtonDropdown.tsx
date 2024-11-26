24f243771c5d705d6657fe66abbbd63c7452022e
Mikk Martin <mikk.martin@gmail.com>
2024-11-26 10:53:54 +0200

    refactoring a bit for migration

--- app/mediabutton/SegmentedButtonDropdown.tsx
+++ app/mediabutton/SegmentedButtonDropdown.tsx
@@ -1,30 +1,41 @@
-import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import { AnimatePresence, motion, useMotionValue } from 'framer-motion';
-import { CheckIcon, ChevronDown, Mic, Plus, Video, X } from 'lucide-react';
-import React, { useEffect, useState } from "react";
-import styles from './MediaButton2.module.scss';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import { AnimatePresence, motion, useMotionValue } from 'framer-motion'
+import React, { useEffect, useState } from 'react'
+import styles from './SegmentedButtonDropdown.module.scss'
+import { Mic, ChevronDown, X, CheckIcon } from 'lucide-react'
+
+// const CheckIcon = (_: { className?: string }) => null
+// const ChevronDown = (_: { className?: string }) => null
+// const Mic = (_: { className?: string }) => null
+// const X = (_: { className?: string }) => null
 
 // const transition = { duration: 0.2 }
-const slowmo = { duration: 2 }
-const bouncy = { type: 'spring', stiffness: 500, damping: 25, mass: 1 }
-const smooth = { type: 'spring', stiffness: 500, damping: 60, mass: 1 }
-const snappy = { type: 'spring', stiffness: 1000, damping: 20, mass: 0.01 }
-const smooth2 = { type: 'spring', stiffness: 800, damping: 60, mass: .1 }
+// const slowmo = { duration: 2 }
+// const bouncy = { type: 'spring', stiffness: 500, damping: 25, mass: 1 }
+// const smooth = { type: 'spring', stiffness: 500, damping: 60, mass: 1 }
+// const snappy = { type: 'spring', stiffness: 1000, damping: 20, mass: 0.01 }
+const smooth2 = { type: 'spring', stiffness: 800, damping: 60, mass: 0.1 }
 
 const transition = smooth2
 // const transition = {}
 
-
-const options = [
-  'MacBook Pro Microphone (Built in microphone)',
-  'Sarah\'s Airpods',
-  'Line in (BlackMagic DeckLink Mini Recorder Audio)'
-]
-
-export function SegmentedButtonDropdown({ children, onOpenChange }: { children: React.ReactNode, onOpenChange?: (open: boolean) => void }) {
+export function SegmentedButtonDropdown({
+  children,
+  options,
+  onValueChange
+}: {
+  children: React.ReactNode
+  onValueChange?: (value: string) => void
+  options: string[]
+}) {
   const [isOpen, setIsOpen] = useState(false)
-  const [selected, setSelected] = useState(options[0])
+  const [selected, _setSelected] = useState(options[0])
   const pointerEvents = useMotionValue<'none' | 'auto'>('none')
+
+  const setSelected = (value: string) => {
+    _setSelected(value)
+    onValueChange?.(value)
+  }
 
   useEffect(() => {
     if (isOpen) {
@@ -41,16 +52,32 @@ export function SegmentedButtonDropdown({ children, onOpenChange }: { children: 
       <DropdownMenu.Root open={isOpen} onOpenChange={setIsOpen} modal={false}>
         <DropdownMenu.Trigger asChild>
           <motion.button className={styles.Trigger}>
-            <motion.div initial={false} animate={{ x: !isOpen ? 0 : -22, rotate: !isOpen ? 0 : 180, opacity: !isOpen ? 1 : 0 }} transition={transition}>
+            <motion.div
+              initial={false}
+              animate={{
+                x: !isOpen ? 0 : -22,
+                rotate: !isOpen ? 0 : 180,
+                opacity: !isOpen ? 1 : 0,
+              }}
+              transition={transition}
+            >
               <ChevronDown className={styles.Icon} />
             </motion.div>
-            <motion.div initial={false} animate={{ x: !isOpen ? 0 : -22, rotate: !isOpen ? -180 : 0, opacity: isOpen ? 1 : 0 }} transition={transition}>
+            <motion.div
+              initial={false}
+              animate={{
+                x: !isOpen ? 0 : -22,
+                rotate: !isOpen ? -180 : 0,
+                opacity: isOpen ? 1 : 0,
+              }}
+              transition={transition}
+            >
               <X className={styles.Icon} />
             </motion.div>
           </motion.button>
         </DropdownMenu.Trigger>
 
-        <AnimatePresence mode="wait">
+        <AnimatePresence>
           {isOpen && (
             <DropdownMenu.Content
               side="top"
@@ -82,11 +109,15 @@ export function SegmentedButtonDropdown({ children, onOpenChange }: { children: 
                       opacity: 0,
                       scale: 0.7,
                       y: 0,
-                    }
+                    },
                   }}
                 />
                 {options.map((option, i) => (
-                  <DropdownMenu.Item key={option} onSelect={() => setSelected(option)} asChild>
+                  <DropdownMenu.Item
+                    key={option}
+                    onSelect={() => setSelected(option)}
+                    asChild
+                  >
                     <motion.div
                       className={styles.Item}
                       data-selected={option === selected}
@@ -97,12 +128,15 @@ export function SegmentedButtonDropdown({ children, onOpenChange }: { children: 
                         visible: {
                           y: 0,
                           opacity: 1,
-                          transition: { ...transition, delay: .01 * (options.length - i) }
+                          transition: {
+                            ...transition,
+                            delay: 0.01 * (options.length - i),
+                          },
                         },
                         hidden: {
                           y: 54 * (options.length - i),
                           opacity: 0,
-                          transition: { duration: 0.1 }
+                          transition: { duration: 0.1 },
                         },
                       }}
                     >
@@ -111,7 +145,11 @@ export function SegmentedButtonDropdown({ children, onOpenChange }: { children: 
                         transition={transition}
                         variants={{
                           visible: { opacity: 1, scale: 1 },
-                          hidden: { opacity: 0, scale: 0.1, transition: { duration: 0.1 } },
+                          hidden: {
+                            opacity: 0,
+                            scale: 0.1,
+                            transition: { duration: 0.1 },
+                          },
                         }}
                       >
                         {option === selected ? <CheckIcon /> : <Mic />}
@@ -120,13 +158,18 @@ export function SegmentedButtonDropdown({ children, onOpenChange }: { children: 
                         className={styles.Label}
                         transition={{ duration: 0.1 }}
                         variants={{
-                          visible: { opacity: 1, transition: { ...transition, delay: 0.1 } },
+                          visible: {
+                            opacity: 1,
+                            transition: { ...transition, delay: 0.1 },
+                          },
                           hidden: { opacity: 0 },
                         }}
                       >
-                        {option === selected &&
-                          <motion.small className={styles.SelectedLabel}>Selected device</motion.small>
-                        }
+                        {option === selected && (
+                          <motion.small className={styles.SelectedLabel}>
+                            Selected device
+                          </motion.small>
+                        )}
                         <span>{option}</span>
                       </motion.div>
                     </motion.div>
@@ -134,22 +177,23 @@ export function SegmentedButtonDropdown({ children, onOpenChange }: { children: 
                 ))}
               </motion.div>
             </DropdownMenu.Content>
-          )}</AnimatePresence>
+          )}
+        </AnimatePresence>
       </DropdownMenu.Root>
       <motion.div
         className={styles.childContainer}
         animate={{
           scale: isOpen ? 0.9 : 1,
           y: !isOpen ? 0 : -30,
-          opacity: !isOpen ? 1 : 0
+          opacity: !isOpen ? 1 : 0,
         }}
         transition={{
           ...transition,
-          opacity: { duration: 0.1 }
+          opacity: { duration: 0.1 },
         }}
       >
         {children}
       </motion.div>
-    </motion.div >
+    </motion.div>
   )
 }
--- app/mediabutton/page.tsx
+++ app/mediabutton/page.tsx
@@ -1,19 +1,22 @@
 'use client'
 
 import { Mic, Plus, Video } from "lucide-react";
-import { SegmentedButtonDropdown } from "./MediaButton2";
+import { SegmentedButtonDropdown } from "./SegmentedButtonDropdown";
 import styles from './page.module.scss';
+
+const micOptions = [ 'MacBook Pro Microphone (Built in microphone)', "Sarah's Airpods", 'Line in (BlackMagic DeckLink Mini Recorder Audio)']
+
 export default function Home() {
   return (
     <div className={styles.root}>
       <video src="https://pub-ac0a62efbd7142ae861959aa8df00996.r2.dev/test.mp4" autoPlay muted loop />
       <div className={styles.testContainer}>
-      <SegmentedButtonDropdown>
+      <SegmentedButtonDropdown options={micOptions}>
         <ButtonWithToolTip>
           <Mic />
         </ButtonWithToolTip>
       </SegmentedButtonDropdown>
-      <SegmentedButtonDropdown>
+      <SegmentedButtonDropdown options={micOptions}>
         <ButtonWithToolTip>
           <Video />
         </ButtonWithToolTip>
