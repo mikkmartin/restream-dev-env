9b2f9e1b49dd0b780aa49d7f961bca899d28324d
Mikk Martin <mikk.martin@gmail.com>
2024-11-27 10:57:30 +0200

    Update page.tsx

--- app/mediabutton/page.tsx
+++ app/mediabutton/page.tsx
@@ -1,24 +1,48 @@
 'use client'
 
-import { Mic, Plus, Video } from "lucide-react";
-import { SegmentedButtonDropdown } from "./SegmentedButtonDropdown";
-import styles from './page.module.scss';
-import { useState } from "react";
+import { Mic, Plus, Video } from 'lucide-react'
+import { SegmentedButtonDropdown } from './SegmentedButtonDropdown'
+import styles from './page.module.scss'
+import { useState } from 'react'
 
-const micOptions = ['MacBook Pro Microphone (Built in microphone)', "Sarah's Airpods", 'Line in (BlackMagic DeckLink Mini Recorder Audio)']
+const micOptions = [
+  'MacBook Pro Microphone (Built in microphone)',
+  "Sarah's Airpods",
+  'Line in (BlackMagic DeckLink Mini Recorder Audio)',
+]
+const addMediaOptions = [
+  'Presentations',
+  'Video storage',
+  'Screen share',
+  'Image',
+  'Extra camera',
+  'Local video',
+  'RTMP source',
+]
 
 export default function Home() {
   const [showVideo, setShowVideo] = useState(true)
   const [lightBackground, setLightBackground] = useState(false)
 
   return (
-    <div className={styles.root} style={{ backgroundColor: !lightBackground ? 'initial' : 'white' }}>
+    <div
+      className={styles.root}
+      style={{ backgroundColor: !lightBackground ? 'initial' : 'white' }}
+    >
       <div>
         <button onClick={() => setShowVideo(!showVideo)}>Toggle Video</button>
-        <button onClick={() => setLightBackground(!lightBackground)}>Toggle Background</button>
+        <button onClick={() => setLightBackground(!lightBackground)}>
+          Toggle Background
+        </button>
       </div>
       <div className={styles.videoPlaceholder}>
-        <video style={{ opacity: !showVideo ? 0 : 1, display: 'block' }} src="https://pub-ac0a62efbd7142ae861959aa8df00996.r2.dev/test.mp4" autoPlay muted loop />
+        <video
+          style={{ opacity: !showVideo ? 0 : 1, display: 'block' }}
+          src="https://pub-ac0a62efbd7142ae861959aa8df00996.r2.dev/test.mp4"
+          autoPlay
+          muted
+          loop
+        />
       </div>
       <div className={styles.testContainer}>
         <SegmentedButtonDropdown options={micOptions} asSegmentedButton>
@@ -38,21 +62,38 @@ export default function Home() {
           <Mic />
         </ButtonWithToolTip>
 
-        <SegmentedButtonDropdown options={micOptions}>
+        <SegmentedButtonDropdown options={addMediaOptions}>
           <ButtonWithToolTip>
             <Plus />
           </ButtonWithToolTip>
         </SegmentedButtonDropdown>
-
         <ButtonWithToolTip>
           <Mic />
         </ButtonWithToolTip>
       </div>
     </div>
-  );
+  )
 }
 
-function ButtonWithToolTip({ children, ...props }: { children: React.ReactNode } & React.ButtonHTMLAttributes<HTMLButtonElement>) {
+{
+  /* <MediaSelect options={micOptions}>
+  <MediaSelectTrigger>
+    <Plus />
+  </MediaSelectTrigger>
+  <MediaSelectContent>
+    {micOptions.map((option) => (
+      <MediaSelectItem key={option}>{option}</MediaSelectItem>
+    ))}
+  </MediaSelectContent>
+</MediaSelect> */
+}
+
+function ButtonWithToolTip({
+  children,
+  ...props
+}: {
+  children: React.ReactNode
+} & React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button className={styles.testButton} {...props}>
       {children}
