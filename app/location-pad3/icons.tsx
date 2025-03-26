90898787cad89a7570a1ab6772031902bf76c973
Mikk Martin <mikk.martin@gmail.com>
2025-03-26 17:56:43 +0000

    Create icons.tsx

---
+++ app/location-pad3/icons.tsx
@@ -0,0 +1,55 @@
+export const CornerTopLeft = () => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M16.5 8.5H10.7857C9.52335 8.5 8.5 9.52335 8.5 10.7857V16.5" stroke="white" strokeWidth={2.28571} strokeLinecap="round" />
+    </svg>
+)
+
+export const CornerTopRight = () => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20 4H12V6H18V12H20V4Z" fill="currentColor" />
+    </svg>
+)
+
+export const CornerBottomLeft = () => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M4 20H12V18H6V12H4V20Z" fill="currentColor" />
+    </svg>
+)
+
+export const CornerBottomRight = () => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20 20H12V18H18V12H20V20Z" fill="currentColor" />
+    </svg>
+)
+
+export const EdgeTop = () => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <line x1="17.0715" y1="9.07142" x2="7.92864" y2="9.07142" stroke="white" strokeWidth={2.28571} strokeLinecap="round" />
+        <line x1="12.4998" y1="10.2143" x2="12.4998" y2="15.9286" stroke="white" strokeWidth={2.28571} strokeLinecap="round" />
+    </svg>
+)
+
+export const EdgeBottom = () => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8 16H16V20H8V16Z" fill="currentColor" />
+    </svg>
+)
+
+export const EdgeLeft = () => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M4 8V16H8V8H4Z" fill="currentColor" />
+    </svg>
+)
+
+export const EdgeRight = () => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M16 8V16H20V8H16Z" fill="currentColor" />
+    </svg>
+)
+
+export const Center = () => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <line x1={12.4998} y1={8.5} x2={12.4998} y2={17.6429} stroke="white" strokeWidth={2.28571} strokeLinecap="round" />
+        <line x1={17.0715} y1={13.0714} x2={7.92864} y2={13.0714} stroke="white" strokeWidth={2.28571} strokeLinecap="round" />
+    </svg>
+) 
