86276b1b58e5706682c9a883324c0b6443e0eb5a
Mikk Martin <mikk.martin@gmail.com>
2025-09-10 11:43:04 +0300

    Update page.tsx

--- app/layout-editor/page.tsx
+++ app/layout-editor/page.tsx
@@ -13,6 +13,7 @@ import styles from './page.module.scss'
 import { useOnClickOutside } from 'usehooks-ts'
 
 export default function LayoutEditor2() {
+  const [editMode, setEditMode] = useState(false)
   const [containerHovered, setContainerHovered] = useState(false)
   const [selected, setSelected] = useState(false)
 
@@ -22,102 +23,172 @@ export default function LayoutEditor2() {
   useOnClickOutside(elRef, () => setSelected(false))
 
   return (
-    <div
-      ref={containerRef}
-      onMouseEnter={() => setContainerHovered(true)}
-      onMouseLeave={() => setContainerHovered(false)}
-      className="w-full aspect-[16/9] overflow-hidden bg-slate-900/30 rounded-xl relative"
-    >
-      <motion.div
-        ref={elRef}
-        onClick={() => setSelected(true)}
+    <div className="flex flex-col w-full">
+      <div
+        ref={containerRef}
+        onMouseEnter={() => setContainerHovered(true)}
+        onMouseLeave={() => setContainerHovered(false)}
         className={cn([
-          'absolute top-1/2 left-1/2 text-white text-9xl font-bold rounded-xl flex flex-col gap-2',
+          'w-full aspect-[16/9] overflow-hidden bg-slate-900 rounded-xl relative outline-4 outline-transparent',
+          editMode && 'outline-blue-500',
         ])}
-        drag
-        dragConstraints={containerRef}
-        dragMomentum={false}
-        style={{
-          x: '-50%',
-          y: '-50%',
-          padding: '8px 16px',
-        }}
       >
-        <span>10:00</span>
         <motion.div
-          initial="initial"
-          whileHover="hover"
-          animate={containerHovered ? 'containerHovered' : undefined}
-          transition={smooth}
-          variants={{
-            initial: { opacity: 1 },
-            hover: { opacity: 1 },
-            selected: { opacity: 1 },
+          ref={elRef}
+          onClick={() => setSelected(true)}
+          className={cn([
+            'absolute top-1/2 left-1/2 text-white text-9xl font-bold rounded-xl flex flex-col gap-2',
+          ])}
+          drag
+          dragConstraints={containerRef}
+          dragMomentum={false}
+          style={{
+            x: '-50%',
+            y: '-50%',
+            padding: '8px 16px',
           }}
-          className={cn(
-            'absolute inset-0 rounded-2xl',
-            containerHovered &&
-              !selected &&
-              'bg-white/10 hover:outline-2 outline-white/60',
-            selected &&
-              'bg-white/10 hover:bg-white/20 hover:outline-2 outline-white/60',
-          )}
-        />
-        {Array.from({ length: 4 }).map((_, i) => (
-          <motion.svg
-            key={i}
-            width="45"
-            height="45"
-            viewBox="0 0 45 45"
-            fill="none"
+        >
+          <span>10:00</span>
+          <motion.div
             initial="initial"
-            animate={[
-              // containerHovered ? 'containerHovered' : '',
-              selected ? 'selected' : '',
-            ]}
-            transition={transition}
-            style={{
-              rotate: i === 0 ? 0 : i === 1 ? 90 : i === 2 ? 180 : 270,
-            }}
-            whileHover="selfHovered"
+            whileHover="hover"
+            animate={containerHovered ? 'containerHovered' : undefined}
+            transition={smooth}
             variants={{
-              initial: {
-                opacity: 0,
-                x: i === 0 || i === 3 ? '-20%' : i === 1 || i === 2 ? '20%' : 0,
-                y: i === 0 || i === 1 ? '-20%' : i === 2 || i === 3 ? '20%' : 0,
-              },
-              containerHovered: {
-                opacity: 0.25,
-                x: i === 0 || i === 3 ? '-60%' : i === 1 || i === 2 ? '60%' : 0,
-                y: i === 0 || i === 1 ? '-60%' : i === 2 || i === 3 ? '60%' : 0,
-              },
-              selected: {
-                opacity: 0.1,
-                x: i === 0 || i === 3 ? '-45%' : i === 1 || i === 2 ? '45%' : 0,
-                y: i === 0 || i === 1 ? '-45%' : i === 2 || i === 3 ? '45%' : 0,
-              },
-              selfHovered: {
-                opacity: 1,
-              },
+              initial: { opacity: 1 },
+              hover: { opacity: 1 },
+              selected: { opacity: 1 },
             }}
-            className={cn([
-              'absolute size-8',
-              i === 0 && 'top-0 left-0',
-              i === 1 && 'right-0 top-0',
-              i === 2 && 'bottom-0 right-0',
-              i === 3 && 'bottom-0 left-0',
-            ])}
-          >
-            <path
-              d="M41 4H40C20.1178 4 4 20.1177 4 40V41"
-              stroke="white"
-              strokeWidth="8"
-              strokeLinecap="round"
-            />
-          </motion.svg>
-        ))}
-      </motion.div>
+            className={cn(
+              'absolute inset-0 rounded-2xl',
+              containerHovered &&
+                !selected &&
+                'bg-white/10 hover:outline-2 outline-white/60',
+              selected &&
+                'bg-white/10 hover:bg-white/20 hover:outline-2 outline-white/60',
+            )}
+          />
+          {Array.from({ length: 4 }).map((_, i) => (
+            <motion.svg
+              key={i}
+              width="45"
+              height="45"
+              viewBox="0 0 45 45"
+              fill="none"
+              initial="initial"
+              animate={[
+                // containerHovered ? 'containerHovered' : '',
+                selected ? 'selected' : '',
+              ]}
+              transition={transition}
+              style={{
+                rotate: i === 0 ? 0 : i === 1 ? 90 : i === 2 ? 180 : 270,
+              }}
+              whileHover="selfHovered"
+              variants={{
+                initial: {
+                  opacity: 0,
+                  x:
+                    i === 0 || i === 3
+                      ? '-20%'
+                      : i === 1 || i === 2
+                        ? '20%'
+                        : 0,
+                  y:
+                    i === 0 || i === 1
+                      ? '-20%'
+                      : i === 2 || i === 3
+                        ? '20%'
+                        : 0,
+                },
+                containerHovered: {
+                  opacity: 0.25,
+                  x:
+                    i === 0 || i === 3
+                      ? '-60%'
+                      : i === 1 || i === 2
+                        ? '60%'
+                        : 0,
+                  y:
+                    i === 0 || i === 1
+                      ? '-60%'
+                      : i === 2 || i === 3
+                        ? '60%'
+                        : 0,
+                },
+                selected: {
+                  opacity: 0.1,
+                  x:
+                    i === 0 || i === 3
+                      ? '-45%'
+                      : i === 1 || i === 2
+                        ? '45%'
+                        : 0,
+                  y:
+                    i === 0 || i === 1
+                      ? '-45%'
+                      : i === 2 || i === 3
+                        ? '45%'
+                        : 0,
+                },
+                selfHovered: {
+                  opacity: 1,
+                },
+              }}
+              className={cn([
+                'absolute size-8',
+                i === 0 && 'top-0 left-0',
+                i === 1 && 'right-0 top-0',
+                i === 2 && 'bottom-0 right-0',
+                i === 3 && 'bottom-0 left-0',
+              ])}
+            >
+              <path
+                d="M41 4H40C20.1178 4 4 20.1177 4 40V41"
+                stroke="white"
+                strokeWidth="8"
+                strokeLinecap="round"
+              />
+            </motion.svg>
+          ))}
+        </motion.div>
+      </div>
+      <div
+        style={{
+          padding: '0.5vw 1vw',
+        }}
+        className={cn(
+          'flex flex-row justify-between w-full',
+          editMode &&
+            'bg-blue-500 outline-4 outline-blue-500 rounded-xl shadow-[0_-20px_0_4px_#2b7fff]',
+        )}
+      >
+        <div className="flex gap-2">
+          <Button>back</Button>
+          <Button>forward</Button>
+        </div>
+        <div>
+          <Button onClick={() => setEditMode(!editMode)}>Edit</Button>
+        </div>
+      </div>
     </div>
+  )
+}
+
+function Button({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode
+  onClick?: () => void
+}) {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-blue-500 text-white px-4 py-2 rounded-md"
+    >
+      {children}
+    </button>
   )
 }
 
