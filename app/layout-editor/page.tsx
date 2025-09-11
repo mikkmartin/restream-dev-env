e3348039bc4d5ac279359cf4d6fd15cc071c804f
Mikk Martin <mikk.martin@gmail.com>
2025-09-11 11:18:33 +0300

    Update page.tsx

--- app/layout-editor/page.tsx
+++ app/layout-editor/page.tsx
@@ -370,7 +370,7 @@ export default function LayoutEditor2() {
                   i === 1 && 'right-0 top-0',
                   i === 2 && 'bottom-0 left-0',
                   i === 3 && 'bottom-0 right-0 cursor-nwse-resize',
-                  !editMode && 'pointer-events-none',
+                  !selected && 'pointer-events-none',
                 ])}
               >
                 <motion.path
